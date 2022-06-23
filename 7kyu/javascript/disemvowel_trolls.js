4b05f61bcc2dbbef24b663f8dffda64b2a597de3
Ryan Tabar <70136507+SirFourier@users.noreply.github.com>
2022-06-23 17:48:49 +0100

    first commit

---
+++ 7kyu/javascript/disemvowel_trolls.js
@@ -0,0 +1,11 @@
+// 7kyu: Disemvowel Trolls
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls'
+// comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, "");
+}
